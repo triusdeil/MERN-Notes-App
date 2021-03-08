c1a6974888b2d727a36e4b9fc96360f6c6360c36
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2021-03-08 07:06:51 -0600

    Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from "react"
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
+import Navigation from './components/Navigation'
+import NotesList from "./components/NotesList";
+import CreateUser from "./components/CreateUser"
+import CreateNotes from './components/CreateNotes'
 function App() {
   return (
-    <h3>Hola Mundo</h3>
+    //Router es un contenedor
+    //Route son las rutas
+    //exact para que coincida exactamente la ruta
+    <Router>
+      <Navigation/>
+      
+      <Route path="/" exact component={NotesList}/>
+
+      <Route path="/edit/:id" component={CreateNotes}/>
+
+      <Route path="/create" component={CreateNotes} />
+
+      <Route path="/user" component={CreateUser} />
+    </Router>
   )
 }
 
